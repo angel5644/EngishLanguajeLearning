1c361671c0e29f8bd441256c9f6c36aa8c601f97
victor.leon <victor_a_75@hotmail.com>
2017-12-02 10:35:29 -0600

    upload initial project

---
+++ ELL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ELL.MvcApplication" Language="C#" %>
---
+++ ELL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELL.Startup))]
+namespace ELL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
