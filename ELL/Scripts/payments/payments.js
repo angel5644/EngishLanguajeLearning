0e3b24f87b38dbe05a5edb5ae7406cfdee53320f
victor.leon <victor_a_75@hotmail.com>
2017-12-15 06:13:31 -0600

    exporting options - payments

---
+++ ELL/Scripts/payments/payments.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    var table = $('#payments-table').DataTable({
+        lengthMenu: [[5, 10, 25, -1], [5, 10, 25, "All"]],
+        order: [0, "asc"], 
+        pageLength: 10,
+        //dom: 'lBfrtip',
+        dom: '<"container-fluid"<"row"<"pull-right"B>><"row"<"col-md-6"l><"col-md-6 pull-right"f>>>rtip',
+        //buttons: [
+        //    'excel','csv',
+        //    'pdf',
+        //    'print'
+        //]
+        buttons: [
+        //{
+        //    extend: 'excel',
+        //    text: '<i class="fa fa-file-excel-o"></i> Excel'
+        //},
+        {
+            extend: 'csv',
+            text: '<i class="fa fa-file-excel-o"></i> CSV'
+        },
+        {
+            extend: 'pdf',
+            text: '<i class="fa fa-file-pdf-o"></i> PDF'
+        },
+        {
+            extend: 'print',
+            text: '<i class="fa fa-file-print-o"></i> Print'
+        }
+        ]
+
+    });
+});
